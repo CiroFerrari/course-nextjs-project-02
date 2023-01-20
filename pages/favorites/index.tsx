e7f9fef8ce62f92d3f8e2314d87a72bf21497edf
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-01-20 11:44:08 -0300

    Favorites page state

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import { Layout } from "../../components/layouts";
+import NoFavorites from "../../components/ui/NoFavorites";
+import { localFavorites } from "../../utils";
 
 export default function FavoritesPage() {
+
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
   return (
     <Layout>
-      <h1>Favoritos Page</h1>
+
+      {
+        (favoritePokemons.length === 0)
+        ? (
+          <NoFavorites />
+        )
+        : (
+          <p>Hay favoritos</p>
+        )
+      }
+
     </Layout>
   );
 }
