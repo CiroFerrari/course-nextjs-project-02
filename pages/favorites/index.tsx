f4bf430a4528dc5072aef5562182904066fe1cd1
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-01-20 12:55:31 -0300

    Favorites page conditional render

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,3 +1,4 @@
+import { Card, Grid } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { Layout } from "../../components/layouts";
 import NoFavorites from "../../components/ui/NoFavorites";
@@ -20,7 +21,21 @@ export default function FavoritesPage() {
           <NoFavorites />
         )
         : (
-          <p>Hay favoritos</p>
+          <Grid.Container gap={2} direction="row" justify="flex-start">
+            {
+              favoritePokemons.map(id => (
+                <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+                  <Card isHoverable isPressable css={{ padding: 10 }}>
+                    <Card.Image
+                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                      width={'100%'}
+                      height={140}
+                    />
+                  </Card>
+                </Grid>
+              ))
+            }
+          </Grid.Container>
         )
       }
 
